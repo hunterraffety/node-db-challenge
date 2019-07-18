a71a33eee50c8d4b8486bc70544d0b91d5dc3e21
hsrsbd <hsraffety@gmail.com>
2019-07-18 15:40:07 -0700

    actions are adding. when grabbing the actions w an id theyre not in the right shape. working on it.

--- projects/projects-model.js
+++ projects/projects-model.js
@@ -3,15 +3,52 @@ const db = require('../data/db-config');
 module.exports = {
   addProject,
   addAction,
-  getProjects
+  getProjects,
+  getProjectById
 };
 
 function addProject(project) {
-  //
+  return db('projects')
+    .insert(project)
+    .then(id => ({ id }))
+    .catch(error => {
+      res.status(500).json(error);
+    });
 }
 
 function addAction(action) {
-  //
+  return db('actions')
+    .insert(action)
+    .then(id => ({ id }))
+    .catch(error => {
+      res.status(500).json(error);
+    });
+}
+
+// need to get actions along with project.
+function getProjectById(id) {
+  return db('projects')
+    .where({ id })
+    .first()
+    .then(project => {
+      if (project) {
+        // return actions table
+        return (
+          db('actions')
+            // find the id, son.
+            .where({ project_id: id })
+            .then(actions => {
+              console.log(actions);
+              return {
+                project,
+                actions
+              };
+            })
+        );
+      } else {
+        return null;
+      }
+    });
 }
 
 function getProjects() {
--- projects/projects-router.js
+++ projects/projects-router.js
@@ -4,12 +4,31 @@ const Projects = require('./projects-model');
 
 const router = express.Router();
 
+router.use(express.json());
+
 router.get('/', async (req, res) => {
   try {
     const projects = await Projects.getProjects();
     res.status(200).json(projects);
   } catch (error) {
     res.status(500).json({ message: `No good, sir.` });
+  }
+});
+
+router.get('/:id', async (req, res) => {
+  const { id } = req.params;
+  console.log(id);
+  try {
+    const project = await Projects.getProjectById(id);
+    if (project) {
+      res.json(project);
+    } else {
+      res
+        .status(404)
+        .json({ message: `Couldn't find a project with that id: ${id}.` });
+    }
+  } catch (error) {
+    res.status(500).json({ message: `There was an error with your request.` });
   }
 });
 
@@ -26,6 +45,25 @@ router.post('/', async (req, res) => {
     } catch (error) {
       res.status(500).json({ message: `Can't create your project, sir.` });
     }
+  }
+});
+
+router.post('/:id/actions', async (req, res) => {
+  const { id } = req.params;
+  const action = req.body;
+  try {
+    const project = await Projects.getProjectById(id);
+    if (project) {
+      const newAction = await Projects.addAction(action);
+      console.log(`action`, action);
+      res.status(201).json(action);
+    } else {
+      res
+        .status(404)
+        .json({ message: `Could not find a project with given ${id}.` });
+    }
+  } catch (error) {
+    res.status(500).json(error);
   }
 });
 
