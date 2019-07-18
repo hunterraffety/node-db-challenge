8527a44daddf7ceff554f72d774e7f070fca41e0
hsrsbd <hsraffety@gmail.com>
2019-07-18 13:48:41 -0700

    migration written out.

--- data/migrations/20190718133015_projects.js
+++ data/migrations/20190718133015_projects.js
@@ -1,3 +1,32 @@
-exports.up = function(knex) {};
+exports.up = function(knex) {
+  return knex.schema
+    .createTable('projects', tbl => {
+      tbl.increments();
 
-exports.down = function(knex) {};
+      tbl
+        .string('project_name', 128)
+        .notNullable()
+        .unique();
+      tbl.string('project_description').notNullable();
+      tbl.boolean('project_complete').notNullable();
+    })
+    .createTable('actions', tbl => {
+      tbl.increments();
+
+      tbl
+        .integer('project_id')
+        .unsigned()
+        .notNullable()
+        .references('id')
+        .inTable('projects')
+        .onDelete('RESTRICT')
+        .onUpdate('CASCADE');
+      tbl.string('action_description').notNullable();
+      tbl.string('action_notes').nullable();
+      tbl.boolean('action_completed').notNullable();
+    });
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists('projects').dropTableIfExists('actions');
+};
